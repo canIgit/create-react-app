5f073fb497791c05568c0b942280789d579040bf
Naresh <naresh@macpro.local>
2019-06-27 13:31:27 +0530

    update eslint and configs

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -219,6 +219,9 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log(chalk.red('VERY IMPORTANT:'))
+  console.log("Create a .env file at the root of your project with REACT_APP_EMPLOYER and REACT_APP_POSITION");
+  console.log();
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  readEnv(){
+    if(!process.env.REACT_APP_EMPLOYER || !process.env.REACT_APP_POSITION){
+      throw new Error('Please define `REACT_APP_EMPLOYER` and `REACT_APP_POSITION`');
+    }
+
+    const employer = process.env.REACT_APP_EMPLOYER;
+    const position = process.env.REACT_APP_POSITION;
+
+    return {employer, position};
+  }
+
+  render(){
+    const {employer, position} = this.readEnv;
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <p className="App-intro">
+          <b>Employer: {employer} </b>
+          <b>position: {position} </b>
+          </p>
+        
+        </div>
+      
+      </div>
+    )
+  }
 }
 
 export default App;
